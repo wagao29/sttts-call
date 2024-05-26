f7462f2cb2a315909243c8a335fcda1152946de9
wagao <wagao1029@gmail.com>
2024-05-27 00:00:00 +0900

    Add support languages

--- constants.ts
+++ constants.ts
@@ -9,6 +9,14 @@ export const ROOM_ID_PATTERN = /^[0-9a-f]{8}$/;
 export const LANGUAGE_LIST = {
   "en-US": "English (en-US)",
   "ja-JP": "Japanese (ja-JP)",
+  "zh-CN": "Chinese (zh-CN)",
+  "ko-KR": "Korean (ko-KR)",
+  "fr-FR": "French (fr-FR)",
+  "de-DE": "German (de-DE)",
+  "es-ES": "Spanish (es-ES)",
+  "it-IT": "Italian (it-IT)",
+  "pt-PT": "Portuguese (pt-PT)",
+  "ru-RU": "Russian (ru-RU)",
 };
 
 // {roomId}-{langCode}
