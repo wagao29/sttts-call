980bb0f93bf2a5a7b3ac9598b38dd87fa0f32876
wagao <wagao1029@gmail.com>
2024-05-28 00:44:21 +0900

    Add html head content

--- ui/layouts/BaseLayout.tsx
+++ ui/layouts/BaseLayout.tsx
@@ -6,9 +6,15 @@ import { Header } from "../templates/Header.tsx";
 
 export const BaseLayout: FC = (props) => {
   return (
-    <html>
+    <html lang="en">
       <head>
         <title>STTTS CALL</title>
+        <meta charset="utf-8" />
+        <meta
+          name="description"
+          content="Call by SpeechToText and TextToSpeech"
+        />
+        <link rel="icon" href="/static/images/favicon.png" />
         <Style>{globalStyle}</Style>
       </head>
       <body>
